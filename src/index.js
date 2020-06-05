5a8d4c89ec19f3ff1ffaaf68e2f97796c5b7cde0
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-06-05 19:31:06 +0300

    Fix types

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { BASE_PATH } from '@/constants/common';
+
 export { default as BaseLayout } from '@/components/BaseLayout.vue';
-export { BASE_PATH } from '@/constants/common';
 export { api, RequestError, configStore } from '@/services';
+
+export const CONSTANTS = {
+  BASE_PATH
+};
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,33 +1,84 @@
 import Vue, { VueConstructor } from 'vue';
 
-declare module '@tager/admin-core' {
-  type QueryParams = { [key: string]: any };
-  type BodyParam = { [key: string]: any } | BodyInit;
+type QueryParams = { [key: string]: any };
+type BodyParam = { [key: string]: any } | BodyInit;
 
-  type RequestParams = {
-    path?: string;
-    body?: BodyParam;
-    params?: QueryParams;
-    absoluteUrl?: string;
-    fetchOptions?: RequestInit;
-  };
+type RequestParams = {
+  path?: string;
+  body?: BodyParam;
+  params?: QueryParams;
+  absoluteUrl?: string;
+  fetchOptions?: RequestInit;
+};
 
-  type ApiRequest = (options: RequestParams) => Promise<any>;
-  type ApiType = {
-    setApiOrigin: (origin: string) => void;
-    get: ApiRequest;
-    post: ApiRequest;
-    put: ApiRequest;
-    patch: ApiRequest;
-    delete: ApiRequest;
-  };
+type ApiRequest = (options: RequestParams) => Promise<any>;
+type ApiType = {
+  setApiOrigin: (origin: string) => void;
+  get: ApiRequest;
+  post: ApiRequest;
+  put: ApiRequest;
+  patch: ApiRequest;
+  delete: ApiRequest;
+  uploadFile: <T>(params: {
+    scenario?: string;
+    file: File;
+    onProgress?: (progressData: {
+      event: ProgressEvent;
+      loaded: number;
+      total: number;
+      progress: number;
+    }) => void;
+  }) => Promise<T>;
+};
 
-  type ConfigService = {
-    setConfig: (config: any) => void;
-    getConfig: <T = any>() => T;
-  };
+type ConfigService = {
+  setConfig: (config: any) => void;
+  getConfig: <T = any>() => T;
+};
 
-  export const BaseLayout: VueConstructor<Vue>;
-  export const api: ApiType;
-  export const configStore: ConfigService;
+type CoreConstants = {
+  BASE_PATH: string;
+};
+
+export type ParsedResponseBody =
+  | boolean
+  | string
+  | { [key: string]: any }
+  | null
+  | undefined;
+
+declare class RequestError extends Error {
+  status: { code: number; text: string };
+  body: ParsedResponseBody;
+
+  constructor(status: { code: number; text: string }, body: ParsedResponseBody);
+}
+
+export const BaseLayout: VueConstructor<Vue>;
+export const api: ApiType;
+export const configStore: ConfigService;
+export const CONSTANTS: CoreConstants;
+
+export type ToastVariant = 'success' | 'warning' | 'danger';
+
+export type ToastParams = {
+  variant: ToastVariant;
+  title: string;
+  body: string;
+};
+
+export type ToastItem = ToastParams & {
+  id: number;
+};
+
+export type ToastFunction = (params: ToastParams) => void;
+
+declare module 'vue/types/vue' {
+  // 3. Declare augmentation for Vue
+  interface Vue {
+    $toast: ToastFunction;
+  }
+  interface VueConstructor {
+    $toast: ToastFunction;
+  }
 }
